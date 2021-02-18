5d6729dfeffb94be5266279a6ad86c1ef8cb3162
noobmaster-00 <arandhawa.cse18@chitkarauniversity.edu.in>
2021-02-18 15:52:47 +0530

    game

---
+++ Assets/Scripts/obstacles.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+
+public class obstacles : MonoBehaviour
+{
+    
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("we hit an obstacle");
+    }
+
+}
---
+++ Assets/Scripts/playercollison.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class playercollison : MonoBehaviour
+{
+    public playermov movement;
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if(collisionInfo.collider.tag=="obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
